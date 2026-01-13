939ddd95bfacc7b9e07c7ef51e36f1f865b97bbc
Denise <deltafoxtrot27@outlook.com>
2026-01-13 08:52:23 -0800

    wk2 challenges

---
+++ wk2/monday1.rb
@@ -0,0 +1,29 @@
+class Email
+  def initialize(headers, body)
+    @headers = headers
+    @body = body
+  end
+
+  def send(to)
+    # TODO: want to log here
+    log()
+    s = connect_to_mail_server()
+    construct_and_send_email(s, to, @headers, @body)
+    close_connection(s)
+  end
+  
+  def log(to)
+    puts (" from: #{@headers[:from]}, to: #{to}, subject: #{@headers[:subject]}")
+  end
+
+end
+
+m = Email.new(
+  {:subject => "Hey!", :from => "beej@beej.us"},
+  "How's it going? Have you heard the latest gossip?"
+)
+to = "chris@example.com"
+m.send("chris@example.com") 
+m.log(to)
+
+#add new code in a separate method and call it within another function so you can test it. This is the sprout method. 
---
+++ wk2/monday2.rb
@@ -0,0 +1,41 @@
+class Email
+  def initialize(headers, body)
+    @headers = headers
+    @body = body
+  end
+
+  def send(to)
+    # TODO: want to log here
+    log()
+    send_fake(to)
+  end
+end
+
+  def log(to)
+    puts (" from: #{@headers[:from]}, to: #{to}, subject: #{@headers[:subject]}")
+  end
+
+  def send_email(to)
+    s = connect_to_mail_server()
+    construct_and_send_email(s, to, @headers, @body)
+    close_connection(s)
+  end
+
+m = Email.new(
+  {:subject => "Hey!", :from => "beej@beej.us"},
+  "How's it going? Have you heard the latest gossip?"
+)
+to = "chris@example.com"
+m.send("chris@example.com")
+
+
+class TestEmail < Email 
+  def send_email(to)
+  end
+end 
+
+e = TestEmail.new()
+e.send()
+
+#modify codebase by extracting offending code and putting into its own method so you can add new code and test it.
+#this is the wrap method 
